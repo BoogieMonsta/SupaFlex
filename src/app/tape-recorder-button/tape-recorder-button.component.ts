4e306b1038565a0dcb5ad805f6fe21aa220d5a1f
BoogieMonsta <boogiemonsta@gmail.com>
2023-05-04 14:23:55 +0200

    red record button

--- src/app/tape-recorder-button/tape-recorder-button.component.ts
+++ src/app/tape-recorder-button/tape-recorder-button.component.ts
@@ -4,24 +4,27 @@ import { AudioTransport } from '../models/AudioTransport';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-tape-recorder-button',
-  templateUrl: './tape-recorder-button.component.html',
-  styleUrls: ['./tape-recorder-button.component.scss']
+    selector: 'app-tape-recorder-button',
+    templateUrl: './tape-recorder-button.component.html',
+    styleUrls: ['./tape-recorder-button.component.scss'],
 })
 export class TapeRecorderButtonComponent implements OnInit {
+    @Input()
+    btnColor = 'gray';
+    @Input()
+    iconName!: string;
+    iconSVG!: SafeHtml;
 
-  @Input()
-  iconName!: string;
-  iconSVG!: SafeHtml;
+    constructor(
+        private httpClient: HttpClient,
+        private sanitizer: DomSanitizer
+    ) {}
 
-  constructor(private httpClient: HttpClient, private sanitizer: DomSanitizer) {}
-  
-  ngOnInit(): void {
-    this.httpClient
-      .get(`assets/svg/${this.iconName}.svg`, { responseType: 'text' })
-      .subscribe(value => {
-        this.iconSVG = this.sanitizer.bypassSecurityTrustHtml(value);
-      });
-  }
-
+    ngOnInit(): void {
+        this.httpClient
+            .get(`assets/svg/${this.iconName}.svg`, { responseType: 'text' })
+            .subscribe((value) => {
+                this.iconSVG = this.sanitizer.bypassSecurityTrustHtml(value);
+            });
+    }
 }
